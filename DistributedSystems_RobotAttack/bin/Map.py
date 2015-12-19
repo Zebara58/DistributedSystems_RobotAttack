dc75f0af489a1347745c2272e263031fe3f1a85f
Zebara58 <kbrennan56@gmail.com>
2015-12-18 19:26:21 -0500

    Comments

--- DistributedSystems_RobotAttack/bin/Map.py
+++ DistributedSystems_RobotAttack/bin/Map.py
@@ -1,8 +1,14 @@
-﻿import logging
+﻿#This is the main map object that is printed to the terminal. 
+#Robots update the main map after they move. The Map object locks for each update.
+#The Map object stops invalid updates to the main map.
+
+import logging
 import random
 import threading
 class Map:
     
+    #Pre: The board size (the x and y size) along with the goal x and y position.
+    #Post: Initializes the main map with the specified size and goal.
     def __init__(self, xSize, ySize, goalX, goalY):
         self.matrix = [["0" for x in range(xSize)] for y in range(ySize)]
         #self.matrix[3][3] = 1
@@ -20,12 +26,15 @@ class Map:
 
         self.lock = threading.Lock()
 
+    #Pre: This takes an x and y coordinate.
+    #Post: This returns True if that position is filled and False otherwise.
     def checkIfFilled(self, x, y):
         if self.matrix[x][y] == "0":
             return False
         else:
             return True
 
+    #Post: This prints out the main map to the termnial.
     def print(self):
         for y in range(self.ySize):
             string = ""
@@ -34,12 +43,13 @@ class Map:
             print(string)
         print("*****************************************")
 
-    #def getLock(self):
-    #    self.lock.acquire()
 
-    #def releaseLock(self):
-    #    self.lock.release()
-
+    #Pre: This takes the current X and Y that the robot tried to move to.
+    #This also takes the previous X and Y position of the robot so that position can
+    #be reset to "0". Finally, the ID of the robot is taken so that the 
+    #robot can be placed on a new spot on the board.
+    #Post: This updates the main map with the new X and Y position of the robot
+    #if that position is valid. Returns True if success and False otherwise. 
     def update(self, curX, curY, prevX, prevY, id):
         self.lock.acquire(True)
         try:  
